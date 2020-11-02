501017a1c57e8bfe353d58f2f8b296f5523c2e88
Ali Villegas <alibryan.villegas@gmail.com>
2020-11-02 13:45:10 -0600

    +added collision detection with cars

--- src/index.js
+++ src/index.js
@@ -1,35 +1,43 @@
 import * as THREE from 'three'
 import * as OrbitControls from 'three-orbitcontrols'
 import * as FBXLoader from 'three-fbx-loader'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import {
+	GLTFLoader
+} from 'three/examples/jsm/loaders/GLTFLoader'
+import {
+	DRACOLoader
+} from 'three/examples/jsm/loaders/DRACOLoader'
 import * as TWEEN from 'three-tween'
 
-import { WEBGL } from './webgl'
+import {
+	WEBGL
+} from './webgl'
 import './modal'
-import { Vector3 } from 'three'
+import {
+	Vector3
+} from 'three'
 //CHECKING IF USER IS ON A SMARTPHONE
 let d = Date();
 let version = d.toString()
-console.log('Version: ' + version)
+//console.log('Version: ' + version)
 function isMobile() {
-	let check = false
-		; (function (a) {
-			if (
-				/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
-					a
-				) ||
-				/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-					a.substr(0, 4)
-				)
+	let check = false;
+	(function (a) {
+		if (
+			/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+				a
+			) ||
+			/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+				a.substr(0, 4)
 			)
-				check = true
-		})(navigator.userAgent || navigator.vendor || window.opera)
+		)
+			check = true
+	})(navigator.userAgent || navigator.vendor || window.opera)
 	return check
 }
 
 if (isMobile()) {
-	console.log('Is on Mobile')
+	//console.log('Is on Mobile')
 	/*
 	var mobileMessage = document.getElementById('mobileMessage')
 	mobileMessage.style.display = 'block'
@@ -39,7 +47,7 @@ if (isMobile()) {
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	*/
 } else {
-	console.log('Is on Desktop')
+	//console.log('Is on Desktop')
 	var game = document.getElementById('game')
 	game.style.display = 'block'
 }
@@ -58,8 +66,7 @@ if (WEBGL.isWebGLAvailable()) {
 	let mainShipColor = 0xffd300
 	let mainShipColorLights = 0x5d1de7
 	let everyXSecondsCounter = 3
-	let spawnRate = 0.1
-
+	let spawnRate = 2
 	//CAR
 	const baseScale = 0.05 * 0.05
 	//OBSTACLES
@@ -99,10 +106,12 @@ if (WEBGL.isWebGLAvailable()) {
 		directionalLight.position.set(1, 0.75, 0.5).normalize()
 		scene.add(directionalLight)
 
-		renderer = new THREE.WebGLRenderer({ antialias: true })
+		renderer = new THREE.WebGLRenderer({
+			antialias: true
+		})
 
 		//controls
-		//controls = new OrbitControls(camera, renderer.domElement)
+		controls = new OrbitControls(camera, renderer.domElement)
 
 		renderer.setPixelRatio(window.devicePixelRatio)
 		renderer.setSize(window.innerWidth, window.innerHeight)
@@ -154,11 +163,11 @@ if (WEBGL.isWebGLAvailable()) {
 			},
 			// called while loading is progressing
 			function (xhr) {
-				console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+				//console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
 			},
 			// called when loading has errors
 			function (error) {
-				console.log('An error happened')
+				//console.log('An error happened')
 			}
 		)
 		increaseSpawnRateEveryXSeconds(5)
@@ -183,8 +192,7 @@ if (WEBGL.isWebGLAvailable()) {
 		musicController.onChange(function (value) {
 			if (value) {
 				backgroundMusic.play();
-			}
-			else {
+			} else {
 				backgroundMusic.pause();
 			}
 		});
@@ -225,21 +233,21 @@ if (WEBGL.isWebGLAvailable()) {
 
 		// onProgress callback
 		// function ( xhr ) {
-		//   console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+		//   //console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
 		// },
 
 		// onError callback
-		function (err) {
-		}
+		function (err) {}
 	);
-	function createGlowingBox() { }
+
+	function createGlowingBox() {}
 	document.onmousemove = function (e) {
 		var raycaster = new THREE.Raycaster() // create once
 		// create once
 		mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1
 		mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
 		raycaster.setFromCamera(mouse, camera)
-		console.log("Cursor at: " + mouse.x + ", " + mouse.y)
+		//console.log("Cursor at: " + mouse.x + ", " + mouse.y)
 		//z rotations +- 0.5
 		if (mainShip) {
 			//mainShip.rotation.z = -0.5
@@ -252,7 +260,7 @@ if (WEBGL.isWebGLAvailable()) {
 	}
 
 	function spawnIncomingCar() {
-		console.log('Spawning incoming Car')
+		//console.log('Spawning incoming Car')
 		gltfLoader.load(
 			// resource URL
 			'static/models/car/scene.gltf',
@@ -264,26 +272,28 @@ if (WEBGL.isWebGLAvailable()) {
 						o.material.emissive = new THREE.Color(Math.random() * 0xffffff)
 					}
 				})
-				//console.log(car)
+				console.log(car)
 				spawnObstacleAtDistance(car.scene)
 			},
 			// called while loading is progressing
 			function (xhr) {
-				console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+				//console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
 			},
 			// called when loading has errors
 			function (error) {
-				console.log('An error happened')
+				//console.log('An error happened')
 			}
 		)
 	}
 
 	function doSomethingAfterXseconds(s) {
-		console.log("Spawning car every x seconds: " + everyXSecondsCounter)
+		//console.log("Spawning car every x seconds: " + everyXSecondsCounter)
 		setTimeout(function () {
-			console.log('Doing something after x time')
+			//console.log('Doing something after x time')
 			var geometry = new THREE.BoxGeometry(1, 1, 1)
-			var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+			var material = new THREE.MeshBasicMaterial({
+				color: 0x00ff00
+			})
 			var cube = new THREE.Mesh(geometry, material)
 			spawnIncomingCar()
 
@@ -291,6 +301,7 @@ if (WEBGL.isWebGLAvailable()) {
 			doSomethingAfterXseconds(everyXSecondsCounter)
 		}, s * 1000)
 	}
+
 	function increaseSpawnRateEveryXSeconds(s) {
 		setTimeout(function () {
 			if (everyXSecondsCounter < 2) {
@@ -310,15 +321,20 @@ if (WEBGL.isWebGLAvailable()) {
 	}
 
 	function spawnObstacleAtDistance(threeDObj) {
+		var bbox = new THREE.Box3().setFromObject(threeDObj);
+		var helper = new THREE.BoxHelper(threeDObj);
+		//scene.add(helper)
 		scene.add(threeDObj)
 		let obstacle = {
 			object3d: threeDObj,
 			animateMovement: true,
 			animateRotation: true,
 			displacementAnimation: null,
-			animationSpeed: 5 - everyXSecondsCounter, //seconds
+			animationSpeed: 5 - everyXSecondsCounter,
+			hitbox: bbox,
+			helper: helper //seconds
 		}
-		console.log("Incoming car Speed: " + obstacle.animationSpeed)
+		//console.log("Incoming car Speed: " + obstacle.animationSpeed)
 		obstacles.push(obstacle)
 	}
 
@@ -327,14 +343,16 @@ if (WEBGL.isWebGLAvailable()) {
 	}
 
 	function onWindowResize() {
-		console.log('Window is being resized')
+		//console.log('Window is being resized')
 		camera.aspect = window.innerWidth / window.innerHeight
 		camera.updateProjectionMatrix()
 
 		renderer.setSize(window.innerWidth, window.innerHeight)
 		render()
 	}
-
+	function carHitObstacle(){
+		console.log(collisionWithCar)
+	}
 	function render() {
 		requestAnimationFrame(render)
 		//console.log(cube.position)
@@ -343,16 +361,25 @@ if (WEBGL.isWebGLAvailable()) {
 		if (obstacles.length > 0 && mainShip) {
 			//console.log(obstacles[0].object3d.position)
 			obstacles.forEach((obs) => {
+				obs.hitbox = new THREE.Box3().setFromObject(obs.object3d);
+				obs.helper.update()
+				if (mainShip) {
+					let mainShipHitBox = new THREE.Box3().setFromObject(mainShip);
+					let mainShipHelper = new THREE.BoxHelper(mainShip, 0xffff00);
+					mainShipHelper.update()
+					//scene.add(mainShipHelper)
+					let collisionWithCar = mainShipHitBox.intersectsBox(obs.hitbox)
+					if (mainShipHitBox.intersectsBox(obs.hitbox)) {
+						carHitObstacle()
+					}
+				}
 				if (obs.animateMovement) {
 					let posNeg = Math.random() < 0.5 ? -1 : 1;
 
 					obs.displacementAnimation = new TWEEN.Tween(obs.object3d.position)
-						.to(
-							{
-								x:
-									camera.position.x + Math.random() * randomIntBetweenXY(-8, 8) * posNeg,
-								y:
-									camera.position.y +
+						.to({
+								x: camera.position.x + Math.random() * randomIntBetweenXY(-8, 8) * posNeg,
+								y: camera.position.y +
 									Math.random() * randomIntBetweenXY(-4, 4) * posNeg,
 								z: camera.position.z,
 							},
@@ -364,7 +391,13 @@ if (WEBGL.isWebGLAvailable()) {
 						.onComplete(function () {
 							/*obs.object3d.geometry.dispose();
 									obs.object3d.material.dispose();*/
+							const index = obstacles.indexOf(obs);
+							if (index > -1) {
+								obstacles.splice(index, 1);
+							}
 							scene.remove(obs.object3d)
+							scene.remove(obs.hitbox)
+							scene.remove(obs.helper)
 							// NEED TO REMOVE ARRAY
 						})
 				}
@@ -378,8 +411,7 @@ if (WEBGL.isWebGLAvailable()) {
 			if (isMobile()) {
 				mainShip.rotation.set(-cursorY * 0.8, 3.1415, -cursorX * 0.5)
 				mainShip.position.set(-cursorX * 0.8, cursorY * 1.5, -95)
-			}
-			else {
+			} else {
 
 				mainShip.rotation.set(-cursorY * 0.8, 3.1415, -cursorX)
 			}
@@ -391,7 +423,7 @@ if (WEBGL.isWebGLAvailable()) {
 				.start(); */
 		}
 
-		//controls.update()
+		controls.update()
 		TWEEN.update()
 	}
 } else {
@@ -406,19 +438,20 @@ function onDocumentTouchEnd(event) {
 
 	mouse.x = (event.changedTouches[0].clientX / window.innerWidth) * 2 - 1;
 	mouse.y = -(event.changedTouches[0].clientY / window.innerHeight) * 2 + 1;
-	console.log("Tap at: " + mouse.x + ", " + mouse.y)
+	//console.log("Tap at: " + mouse.x + ", " + mouse.y)
 
 	cursorX = mouse.x
 	cursorY = mouse.y
 	//raycaster.setFromCamera(mouse, camera);
 	//const intersects = raycaster.intersectObjects(yourObject3D);
 }
+
 function handleMove(event) {
 	event.preventDefault();
 
 	mouse.x = (event.changedTouches[0].clientX / window.innerWidth) * 2 - 1;
 	mouse.y = -(event.changedTouches[0].clientY / window.innerHeight) * 2 + 1;
-	console.log("Tap at: " + mouse.x + ", " + mouse.y)
+	//console.log("Tap at: " + mouse.x + ", " + mouse.y)
 
 	cursorX = mouse.x
 	cursorY = mouse.y
